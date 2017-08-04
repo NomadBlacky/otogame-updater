65f879532ef7bd04d8f84e2ba082aa0b3d96cb18
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-08-04 16:04:55 +0900

    Add `level: Int` to Stage

--- src/main/scala/org/nomad/blacky/otogameupdater/game/cbrev/Stage.scala
+++ src/main/scala/org/nomad/blacky/otogameupdater/game/cbrev/Stage.scala
@@ -3,4 +3,9 @@ package org.nomad.blacky.otogameupdater.game.cbrev
 /**
   * Created by blacky on 17/07/19.
   */
-case class Stage(music: MusicInList, difficulty: Difficulty, notes: Int)
+case class Stage(
+  music: MusicInList,
+  difficulty: Difficulty,
+  level: Int,
+  notes: Int
+)
