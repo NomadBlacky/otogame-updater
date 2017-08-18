a20a71b19c942b1a815b0ae21d1a1cdda24909b6
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-08-18 12:32:16 +0900

    [WIP] update_user_data_stream.Handler

--- src/main/scala/org/nomadblacky/otogameupdater/game/cbrev/lambda/update_user_data_stream/Handler.scala
+++ src/main/scala/org/nomadblacky/otogameupdater/game/cbrev/lambda/update_user_data_stream/Handler.scala
@@ -1,9 +1,11 @@
 package org.nomadblacky.otogameupdater.game.cbrev.lambda.update_user_data_stream
 
+import com.amazonaws.services.dynamodbv2.model.{AttributeValue, StreamRecord}
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 
 import scala.beans.BeanProperty
+import scala.collection.JavaConverters._
 
 case class Response(
   @BeanProperty message: String,
@@ -13,6 +15,35 @@ case class Response(
 class Handler extends RequestHandler[DynamodbEvent, Response] {
 
   override def handleRequest(input: DynamodbEvent, context: Context): Response = {
+    val logger = context.getLogger
+
+    input.getRecords.asScala.map(_.getDynamodb).foreach(eachRecord)
+
     Response("OK", input)
+  }
+
+  def eachRecord(record: StreamRecord): Unit = {
+    for {
+      oldImage <- Option(record.getOldImage)
+      newImage <- Option(record.getNewImage)
+    } {
+      getTweetText(oldImage.asScala, newImage.asScala).foreach { text =>
+        // TODO: Invoke `tweeting`
+      }
+    }
+  }
+
+  def getTweetText(
+    oldImage: collection.Map[String, AttributeValue],
+    newImage: collection.Map[String, AttributeValue]
+  ): Option[String] = {
+    for {
+      oldRp <- oldImage.get("rankPoint").flatMap(v => Option(v.getN)).map(_.toDouble)
+      newRp <- newImage.get("rankPoint").flatMap(v => Option(v.getN)).map(_.toDouble)
+      diff  <- Some(newRp - oldRp).filter(0.0 < _)
+    } yield {
+      // TODO: Implement it
+      s"oldRp: $oldRp, newRp: $newRp, diff: $diff"
+    }
   }
 }
