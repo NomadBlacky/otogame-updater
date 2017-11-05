541635a8792fd67bb81cdfbaf5f1000e45a905f6
NomadBlacky <hourou.hamurinzu@gmail.com>
2017-11-05 19:09:20 +0900

    Fix property name of MusicPlayData

--- src/main/scala/org/nomadblacky/otogameupdater/game/cbrev/model/MusicPlayData.scala
+++ src/main/scala/org/nomadblacky/otogameupdater/game/cbrev/model/MusicPlayData.scala
@@ -1,6 +1,6 @@
 package org.nomadblacky.otogameupdater.game.cbrev.model
 
 case class MusicPlayData(
-  musicInList: MusicDetail,
+  musicDetail: MusicDetail,
   playScores: Map[Difficulty, PlayScore]
 )
